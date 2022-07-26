e36e77fdb0a61ab68b62d9ffb3b63ada4f606b77
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-26 18:08:11 +0200

    added BaseHeader

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader'
+}
+</script>
+
+<style scoped>
+    header{
+        height: 50px;
+        background-color: #2e3a46;
+    }
+</style>
