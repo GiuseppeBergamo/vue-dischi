734097840f48c63ab5fc1b2d2480327e02ddb577
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-26 18:08:54 +0200

    started Main

---
+++ src/components/TheMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <div class="container">
+
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    main{
+        
+    }
+</style>
