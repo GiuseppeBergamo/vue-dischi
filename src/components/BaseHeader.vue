543a19932d51aa6c1007da9a44ad463acdc73fdb
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-26 20:57:26 +0200

    added spotify logo

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header>
-
+  <header class="container-fluid py-1 d-flex flex-start">
+    <img src="https://th.bing.com/th/id/R.2ebc6c77ba84d7194d4a8f6a7334571e?rik=2ffoY4RHjXWJ2w&pid=ImgRaw&r=0" alt="spotify-logo" class="img-fluid">
   </header>
 </template>
 
