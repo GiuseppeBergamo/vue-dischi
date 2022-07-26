c2c0c81e6e07710ef58a62fa0da88185fe07fcd9
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-26 20:55:05 +0200

    Added TheMain

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <BaseHeader></BaseHeader>
+    <TheMain></TheMain>
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/BaseHeader.vue';
-
+import TheMain from './components/TheMain.vue';
 
 export default {
   name: 'App',
   components: {
-    BaseHeader
+    BaseHeader,
+    TheMain
 }
 }
 </script>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,19 +1,23 @@
 <template>
   <main>
-    <div class="container">
-
+    <div class="container py-4">
+        <AlbumSection></AlbumSection>
     </div>
   </main>
 </template>
 
 <script>
+import AlbumSection from './AlbumSection.vue';
 export default {
-
+    name: 'TheMain',
+    components: {
+        AlbumSection,
+    }
 }
 </script>
 
 <style scoped>
     main{
-        
+        background-color: #1e2d3b;
     }
 </style>
