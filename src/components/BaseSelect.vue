9852db58227e9c29b816307cd694fd0b70fbfa5c
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-27 20:15:52 +0200

    started BaseSelect components

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,18 @@
+<template>
+  <select>
+    <option v-for="genre in albums" :key="album.title" v-model=""></option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'BaseSelect',
+    props: {
+        albums: Array
+    }
+}
+</script>
+
+<style>
+
+</style>
